ababca23ce3186cc8848a5161420defb6bf02c58
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2021-11-05 11:06:37 +0100

    modem_trace: Initial draft of API
    Initial draft of modem_trace API.
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

---
+++ tests/lib/modem_trace/src/modem_trace_test.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+#include <modem/modem_trace.h>
+
+extern int unity_main(void);
+
+void main(void)
+{
+	(void)unity_main();
+}
