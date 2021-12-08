275074795db20a357e1ce6004600702677af3363
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2021-12-08 16:16:11 +0100

    wip: First stages

---
+++ lib/modem_trace/modem_trace.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+int modem_trace_init(void)
+{
+	return 0;
+}
--- tests/lib/modem_trace/src/modem_trace_test.c
+++ tests/lib/modem_trace/src/modem_trace_test.c
@@ -3,9 +3,15 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
+#include <unity.h>
 #include <modem/modem_trace.h>
 
 extern int unity_main(void);
+
+void test_modem_trace_init(void)
+{
+	TEST_ASSERT_EQUAL(modem_trace_init(), 0);
+}
 
 void main(void)
 {
