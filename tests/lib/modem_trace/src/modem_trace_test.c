ffdc383b5e9b6dea841e46b0270ea3ecd2b6aee9
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2021-12-08 21:29:45 +0100

    WIP; cmake changes to compile in the uart headers

--- tests/lib/modem_trace/src/modem_trace_test.c
+++ tests/lib/modem_trace/src/modem_trace_test.c
@@ -4,12 +4,17 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 #include <unity.h>
+#include <stdbool.h>
 #include <modem/modem_trace.h>
 
 extern int unity_main(void);
 
+bool runtime_CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_UART;
+
 void test_modem_trace_init(void)
 {
+	runtime_CONFIG_NRF_MODEM_LIB_TRACE_MEDIUM_UART = true;
+
 	TEST_ASSERT_EQUAL(modem_trace_init(), 0);
 }
 
