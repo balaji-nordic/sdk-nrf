bed3fd531e31954f004456fa3917db2173f5e251
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2022-01-05 14:13:21 +0100

    Configure 2 LEDs

--- samples/peripheral/gpio_to_led/src/main.c
+++ samples/peripheral/gpio_to_led/src/main.c
@@ -12,23 +12,28 @@
 
 LOG_MODULE_REGISTER(app);
 
-#define INPUT_PIN 17
-#define OUTPUT_PIN 2
+#define INPUT_PIN_1 17
+#define INPUT_PIN_2 18
+#define LED1_PIN 2
+#define LED2_PIN 3
 
-static void inp_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
-{
-	LOG_INF("Input");
-}
 
-void main(void)
+/* Configure endpoints of the channel so that the input pin polarity is followed by the output
+ * pin.
+ */
+static void connect_pins(uint8_t input_pin, uint8_t output_pin)
 {
 	nrfx_err_t err;
+	static bool gpiote_init_done = false;
 
-	err = nrfx_gpiote_init(0);
-	if (err != NRFX_SUCCESS) {
-		LOG_ERR("nrfx_gpiote_init error: %08x", err);
-		return;
+	if (!gpiote_init_done) {
+		err = nrfx_gpiote_init(0);
+		if (err != NRFX_SUCCESS) {
+			LOG_ERR("nrfx_gpiote_init error: %08x", err);
+			return;
+		}
 	}
+	gpiote_init_done = true;
 
 	nrfx_gpiote_in_config_t const in_config = {
 		.sense = NRF_GPIOTE_POLARITY_TOGGLE,
@@ -38,10 +43,7 @@ void main(void)
 		.skip_gpio_setup = false,
 	};
 
-	/* Initialize input pin to generate event on high to low transition
-	 * (falling edge)
-	 */
-	err = nrfx_gpiote_in_init(INPUT_PIN, &in_config, &inp_handler);
+	err = nrfx_gpiote_in_init(input_pin, &in_config, NULL);
 	if (err != NRFX_SUCCESS) {
 		LOG_ERR("nrfx_gpiote_in_init error: %08x", err);
 		return;
@@ -49,23 +51,21 @@ void main(void)
 
 	nrfx_gpiote_out_config_t const out_config = {
 		.action = NRF_GPIOTE_POLARITY_TOGGLE,
-		.init_state = 1,
+		.init_state = NRF_GPIOTE_INITIAL_VALUE_LOW,
 		.task_pin = true,
 	};
 
 	/* Initialize output pin. SET task will turn the LED on,
 	 * CLR will turn it off and OUT will toggle it.
 	 */
-	err = nrfx_gpiote_out_init(OUTPUT_PIN, &out_config);
+	err = nrfx_gpiote_out_init(output_pin, &out_config);
 	if (err != NRFX_SUCCESS) {
 		LOG_ERR("nrfx_gpiote_out_init error: %08x", err);
 		return;
 	}
 
-	nrfx_gpiote_in_event_enable(INPUT_PIN, true);
-	nrfx_gpiote_out_task_enable(OUTPUT_PIN);
-
-	LOG_INF("nrfx_gpiote initialized");
+	nrfx_gpiote_in_event_enable(input_pin, true);
+	nrfx_gpiote_out_task_enable(output_pin);
 
 	/* Allocate a DPPI channel. */
 	uint8_t channel;
@@ -80,8 +80,8 @@ void main(void)
 	 * the button is pressed, the LED pin will be toggled.
 	 */
 	nrfx_gppi_channel_endpoints_setup(channel,
-		nrfx_gpiote_in_event_addr_get(INPUT_PIN),
-		nrfx_gpiote_out_task_addr_get(OUTPUT_PIN));
+		nrfx_gpiote_in_event_addr_get(input_pin),
+		nrfx_gpiote_out_task_addr_get(output_pin));
 
 	err = nrfx_dppi_channel_enable(channel);
 	if (err != NRFX_SUCCESS) {
@@ -89,5 +89,11 @@ void main(void)
 		return;
 	}
 
-	LOG_INF("(D)PPI configured, leaving main()");
+	LOG_INF("Input pin %d connected to output pin %d.", input_pin, output_pin);
+}
+
+void main(void)
+{
+	connect_pins(INPUT_PIN_1, LED1_PIN);
+	connect_pins(INPUT_PIN_2, LED2_PIN);
 }
